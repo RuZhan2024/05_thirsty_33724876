8d134e4e751e8f4a558f99ff114d542c8ddd49c5
MackSure <bzruzhan@163.com>
2025-10-29 12:43:24 +0000

    Add dynamic variable to the home page

---
+++ routes/main.js
@@ -0,0 +1,26 @@
+// Create a new router
+const express = require("express");
+const router = express.Router();
+
+ // Define our data
+ const shopData = {shopName: "The Thirsty Student"};
+
+
+// Handle the main routes
+// TODO
+router.get("/", (req, res) => {
+    res.render("index.ejs", {...shopData})
+}); 
+
+router.get("/about", (req, res) => {
+    res.render("about.ejs")
+
+});
+
+router.get("/search", (req, res) => {
+    res.render("search.ejs")
+
+});
+// Export the router object so index.js can access it
+module.exports = router;
+
